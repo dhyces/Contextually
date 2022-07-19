a8fffb08713c79895c94f2e9873ff73ff6d18883
dhyces <pokmon987@gmail.com>
2022-07-19 19:54:16 -0400

    Test context render
    initial test to render a texture

--- Forge/src/main/java/dhyces/contextually/client/gui/ContextGuiOverlay.java
+++ Forge/src/main/java/dhyces/contextually/client/gui/ContextGuiOverlay.java
@@ -1,12 +1,30 @@
 package dhyces.contextually.client.gui;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
+import dhyces.contextually.ContextuallyClient;
+import dhyces.contextually.ContextuallyCommon;
+import dhyces.contextually.client.textures.KeyMappingTextureManager;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 public class ContextGuiOverlay implements IGuiOverlay {
+
     @Override
     public void render(ForgeGui gui, PoseStack poseStack, float partialTick, int width, int height) {
-        gui.
+        gui.setupOverlayRenderState(true, false, KeyMappingTextureManager.KEYS);
+        var mc = gui.getMinecraft();
+        var clientPlayer = mc.player;
+        var hitResult = mc.hitResult;
+        if (hitResult instanceof BlockHitResult blockHitResult && !blockHitResult.getType().equals(HitResult.Type.MISS)) {
+            var key = ContextuallyClient.getKeyTextureManager().get(ContextuallyCommon.modloc("a_key"));
+            GuiComponent.blit(poseStack, width/2, height/2, gui.getBlitOffset(), 8, 8, key);
+        } else if (hitResult instanceof EntityHitResult entityHitResult && !entityHitResult.getType().equals(HitResult.Type.MISS)) {
+
+        }
     }
 }
