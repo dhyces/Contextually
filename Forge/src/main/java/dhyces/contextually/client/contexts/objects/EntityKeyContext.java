9028063af824bfa1ba87c47bec5cfd0534f6db15
dhyces <pokmon987@gmail.com>
2022-07-21 12:39:57 -0400

    Update EntityKeyContext.java

--- Forge/src/main/java/dhyces/contextually/client/contexts/objects/EntityKeyContext.java
+++ Forge/src/main/java/dhyces/contextually/client/contexts/objects/EntityKeyContext.java
@@ -1,5 +1,6 @@
 package dhyces.contextually.client.contexts.objects;
 
+import com.google.gson.JsonObject;
 import dhyces.contextually.client.contexts.conditions.Condition;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
@@ -11,5 +12,18 @@ public class EntityKeyContext extends AbstractKeyContext<Entity> {
 
     public EntityKeyContext(@NotNull ResourceLocation id, @NotNull Set<ResourceLocation> keys, @NotNull Set<Condition> conditions) {
         super(id, keys, conditions);
+    }
+
+    public static class Serializer implements ISerializer<Entity, EntityKeyContext> {
+
+        @Override
+        public EntityKeyContext deserialize(ResourceLocation id, JsonObject json) {
+            return null;
+        }
+
+        @Override
+        public JsonObject serialize(ResourceLocation id, EntityKeyContext context) {
+            return null;
+        }
     }
 }
