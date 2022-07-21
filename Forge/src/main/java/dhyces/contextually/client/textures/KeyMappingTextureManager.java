45fd0aba2d0f11ff4b2ae1a9bfffe52ba7d65380
dhyces <pokmon987@gmail.com>
2022-07-20 21:41:56 -0400

    Remove old, commented out code

--- Forge/src/main/java/dhyces/contextually/client/textures/KeyMappingTextureManager.java
+++ Forge/src/main/java/dhyces/contextually/client/textures/KeyMappingTextureManager.java
@@ -1,8 +1,6 @@
 package dhyces.contextually.client.textures;
 
-import com.google.common.collect.Maps;
 import dhyces.contextually.ContextuallyCommon;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.resources.TextureAtlasHolder;
@@ -29,11 +27,5 @@ public class KeyMappingTextureManager extends TextureAtlasHolder {
     @Override
     protected Stream<ResourceLocation> getResourcesToLoad() {
         return KeyConstants.KEY_LOCATIONS.values().stream();
-//        var resourceManager = Minecraft.getInstance().getResourceManager();
-//        var resourcesMap = resourceManager.listResources("textures/key", c -> c.getPath().endsWith(".png"));
-//        // TODO: make this better. Not a big fan of having so much abstract string manipulation. One idea is to have a
-//        //  sort of "registration" where we store a map and manually add all of the resource locations for each key.
-//        //  This has the benefit of being able to immediately access an RL from an InputConstant.
-//        return resourcesMap.keySet().stream().map(c -> ContextuallyCommon.modloc(c.getPath().substring(13, c.getPath().length()-4)));
     }
 }
