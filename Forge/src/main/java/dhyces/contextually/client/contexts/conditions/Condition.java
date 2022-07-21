f9be04f90600d1fecea7d6631a04573b52e09c3c
dhyces <pokmon987@gmail.com>
2022-07-21 10:56:54 -0400

    Update Condition.java
    - Add target versions of hand item checks

--- Forge/src/main/java/dhyces/contextually/client/contexts/conditions/Condition.java
+++ Forge/src/main/java/dhyces/contextually/client/contexts/conditions/Condition.java
@@ -5,23 +5,92 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.item.Item;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class Condition implements IConditionPredicate {
 
-    public static class HeldItemNbtCondition extends Condition {
+    public static class TargetHeldItemNbtCondition extends Condition {
 
         final CompoundTag tag;
-        final InteractionHand hand;
+        @Nullable final InteractionHand hand;
 
-        public HeldItemNbtCondition(CompoundTag tag, InteractionHand hand) {
+        public TargetHeldItemNbtCondition(CompoundTag tag, @Nullable InteractionHand hand) {
             this.tag = tag;
             this.hand = hand;
         }
 
         @Override
         public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
+            if (target instanceof LivingEntity livingEntity) {
+                if (hand == null) {
+                    return livingEntity.getMainHandItem().getTag().equals(tag) || livingEntity.getOffhandItem().getTag().equals(tag);
+                }
+                return livingEntity.getItemInHand(hand).getTag().equals(tag);
+            }
+            return false;
+        }
+    }
+
+    public static class TargetHeldItemCondition extends Condition {
+
+        final Item item;
+        @Nullable final InteractionHand hand;
+
+        public TargetHeldItemCondition(Item item, @Nullable InteractionHand hand) {
+            this.item = item;
+            this.hand = hand;
+        }
+
+        @Override
+        public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
+            if (target instanceof LivingEntity livingEntity) {
+                if (hand == null) {
+                    return livingEntity.getMainHandItem().is(item) || livingEntity.getOffhandItem().is(item);
+                }
+                return livingEntity.getItemInHand(hand).is(item);
+            }
+            return false;
+        }
+    }
+
+    public static class PlayerHeldItemNbtCondition extends Condition {
+
+        final CompoundTag tag;
+        @Nullable final InteractionHand hand;
+
+        public PlayerHeldItemNbtCondition(CompoundTag tag, @Nullable InteractionHand hand) {
+            this.tag = tag;
+            this.hand = hand;
+        }
+
+        @Override
+        public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
+            if (hand == null) {
+                return player.getMainHandItem().getTag().equals(tag) || player.getOffhandItem().getTag().equals(tag);
+            }
             return player.getItemInHand(hand).getTag().equals(tag);
+        }
+    }
+
+    public static class PlayerHeldItemCondition extends Condition {
+
+        final Item item;
+        @Nullable final InteractionHand hand;
+
+        public PlayerHeldItemCondition(Item item, @Nullable InteractionHand hand) {
+            this.item = item;
+            this.hand = hand;
+        }
+
+        @Override
+        public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
+            if (hand == null) {
+                return player.getMainHandItem().is(item) || player.getOffhandItem().is(item);
+            }
+            return player.getItemInHand(hand).is(item);
         }
     }
 
@@ -62,20 +131,6 @@ public abstract class Condition implements IConditionPredicate {
         }
     }
 
-    public static class NotCondition extends Condition {
-
-        final Condition condition;
-
-        public NotCondition(Condition condition) {
-            this.condition = condition;
-        }
-
-        @Override
-        public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
-            return !condition.test(target, level, player);
-        }
-    }
-
     public static class VillagerProfessionCondition extends Condition {
 
         final String profession;
@@ -90,6 +145,20 @@ public abstract class Condition implements IConditionPredicate {
                 return profession.equals(villager.getVillagerData().getProfession().name());
             }
             return false;
+        }
+    }
+
+    public static class NotCondition extends Condition {
+
+        final Condition condition;
+
+        public NotCondition(Condition condition) {
+            this.condition = condition;
+        }
+
+        @Override
+        public boolean test(Object target, ClientLevel level, AbstractClientPlayer player) {
+            return !condition.test(target, level, player);
         }
     }
 }
