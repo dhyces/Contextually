f3071613a62b4f6d89af556f49d7ae349cbf1905
dhyces <pokmon987@gmail.com>
2022-07-21 21:57:28 -0400

    Rename ContextSerializer

---
+++ Forge/src/main/java/dhyces/contextually/client/contexts/objects/serializers/IContextSerializer.java
@@ -0,0 +1,52 @@
+package dhyces.contextually.client.contexts.objects.serializers;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.datafixers.util.Pair;
+import dhyces.contextually.client.contexts.KeyContextLoader;
+import dhyces.contextually.client.contexts.conditions.Condition;
+import dhyces.contextually.client.contexts.keys.Key;
+import dhyces.contextually.client.contexts.objects.IKeyContext;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+
+public interface IContextSerializer<E, T extends IKeyContext<E>> {
+
+    Pair<Collection<E>, T> deserialize(@NotNull ResourceLocation id, @NotNull JsonObject json);
+
+    JsonObject serialize(@NotNull ResourceLocation id, @NotNull Pair<Collection<E>, T> context);
+
+    default Set<Condition> readConditions(@Nullable JsonArray conditionArray) {
+        ImmutableSet.Builder<Condition> builder = ImmutableSet.builder();
+        if (conditionArray != null) {
+            for (JsonElement o : conditionArray) {
+                try {
+                    builder.add(KeyContextLoader.deserializeCondition(o.getAsJsonObject()));
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.build();
+    }
+
+    default Set<Key> readKeys(@NotNull JsonArray keyArray) {
+        Objects.requireNonNull(keyArray);
+        ImmutableSet.Builder<Key> builder = ImmutableSet.builder();
+        for (JsonElement o : keyArray) {
+            try {
+                builder.add(KeyContextLoader.deserializeKey(o.getAsJsonObject()));
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+        return builder.build();
+    }
+}
