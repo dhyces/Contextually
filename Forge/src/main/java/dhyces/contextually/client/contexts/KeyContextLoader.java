158031f8f909f11ef76228fc3f92881366edce13
dhyces <pokmon987@gmail.com>
2022-07-21 22:00:37 -0400

    We're getting serial

---
+++ Forge/src/main/java/dhyces/contextually/client/contexts/KeyContextLoader.java
@@ -0,0 +1,114 @@
+package dhyces.contextually.client.contexts;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.datafixers.util.Pair;
+import dhyces.contextually.ContextuallyCommon;
+import dhyces.contextually.client.contexts.conditions.Condition;
+import dhyces.contextually.client.contexts.conditions.serializers.ConditionSerializers;
+import dhyces.contextually.client.contexts.conditions.serializers.IConditionSerializer;
+import dhyces.contextually.client.contexts.keys.Key;
+import dhyces.contextually.client.contexts.keys.serializers.IKeySerializer;
+import dhyces.contextually.client.contexts.keys.serializers.KeySerializers;
+import dhyces.contextually.client.contexts.objects.BlockKeyContext;
+import dhyces.contextually.client.contexts.objects.EntityKeyContext;
+import dhyces.contextually.client.contexts.objects.GlobalKeyContext;
+import dhyces.contextually.client.contexts.objects.IKeyContext;
+import dhyces.contextually.client.contexts.objects.serializers.IContextSerializer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.*;
+
+public class KeyContextLoader {
+
+    private static final ImmutableMap<String, IKeySerializer> KEY_SERIALIZERS_MAP;
+    private static final ImmutableSet<IConditionSerializer> CONDITION_SERIALIZERS;
+    private static final ImmutableMap<ResourceLocation, IContextSerializer<?, ?>> CONTEXT_SERIALIZERS;
+    private final ResourceManager resourceManager;
+
+    final String fileExtension = ".json";
+
+    public KeyContextLoader(ResourceManager resourceManager) {
+        this.resourceManager = resourceManager;
+    }
+
+    public List<Pair<? extends Collection<?>, ? extends IKeyContext<?>>> load() {
+        List<Pair<? extends Collection<?>, ? extends IKeyContext<?>>> read = new LinkedList<>();
+        for (Map.Entry<ResourceLocation, Resource> entry : resourceManager.listResources("contexts", resourceLocation -> resourceLocation.getPath().endsWith(".json")).entrySet()) {
+            try(var reader = entry.getValue().openAsReader()) {
+                var jsonElement = JsonParser.parseReader(reader);
+                try {
+                    var obj = jsonElement.getAsJsonObject();
+                    var loader = obj.get("loader").getAsString();
+                    var loaderKey = loader.contains(":") ? ResourceLocation.of(loader, ':') : ContextuallyCommon.modloc(loader);
+                    var serializer = CONTEXT_SERIALIZERS.get(loaderKey);
+                    try {
+                        Objects.requireNonNull(serializer, "Loader \"" + loaderKey + "\" does not exist.");
+                        var path = entry.getKey();
+                        var id = new ResourceLocation(path.getNamespace(), path.getPath().substring("contexts/".length(), path.getPath().indexOf(fileExtension)).replaceAll("\\/", "."));
+                        var context = serializer.deserialize(id, obj);
+                        Objects.requireNonNull(context, "Deserializer \"" + loaderKey + "\" returned null context.");
+                        read.add(context);
+                    } catch (NullPointerException e) {
+                        ContextuallyCommon.LOGGER.error(e.getMessage());
+                    }
+                } catch (NullPointerException e) {
+                    throw new IllegalStateException("Context json must contain the \"loader\" key with a valid loader.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return read;
+    }
+
+    @NotNull
+    public static Condition deserializeCondition(JsonObject conditionObject) {
+        Objects.requireNonNull(conditionObject);
+        for (IConditionSerializer serializer : CONDITION_SERIALIZERS) {
+            if (serializer.getId().toString().equals(conditionObject.get("condition").getAsString())) {
+                return serializer.deserialize(conditionObject);
+            }
+        }
+        throw new IllegalArgumentException("No serializer for condition: " + conditionObject + " exists.");
+    }
+
+    @NotNull
+    public static Key deserializeKey(JsonObject keyObject) {
+        Objects.requireNonNull(keyObject);
+        for (String e : keyObject.keySet()) {
+            if (KEY_SERIALIZERS_MAP.containsKey(e)) {
+                var serializer = KEY_SERIALIZERS_MAP.get(e);
+                return serializer.deserialize(keyObject.get(e));
+            }
+        }
+        throw new IllegalArgumentException("No serializer for condition: " + keyObject + " exists.");
+    }
+
+    static {
+        ImmutableMap.Builder<ResourceLocation, IContextSerializer<?, ?>> contextSerializerBuilder = ImmutableMap.builder();
+        contextSerializerBuilder.put(ContextuallyCommon.modloc("block_context"), new BlockKeyContext.Serializer());
+        contextSerializerBuilder.put(ContextuallyCommon.modloc("entity_context"), new EntityKeyContext.Serializer());
+        contextSerializerBuilder.put(ContextuallyCommon.modloc("global_context"), new GlobalKeyContext.Serializer());
+
+        // TODO: Fire registry event
+        CONTEXT_SERIALIZERS = contextSerializerBuilder.build();
+
+        ImmutableSet.Builder<IConditionSerializer> conditionSerializerBuilder = ImmutableSet.builder();
+        conditionSerializerBuilder.add(ConditionSerializers.PLAYER_HELD_ITEM_SERIALIZER);
+        conditionSerializerBuilder.add(ConditionSerializers.PLAYER_HELD_BLOCK_SERIALIZER);
+        CONDITION_SERIALIZERS = conditionSerializerBuilder.build();
+
+        ImmutableMap.Builder<String, IKeySerializer> keySerializerBuilder = ImmutableMap.builder();
+        keySerializerBuilder.put("mapping", KeySerializers.MAPPING_KEY_SERIALIZER);
+//        keySerializerBuilder.put("key", KeySerializers.KEYCODE_KEY_SERIALIZER);
+        keySerializerBuilder.put("texture", KeySerializers.TEXTURE_KEY_SERIALIZER);
+        KEY_SERIALIZERS_MAP = keySerializerBuilder.build();
+    }
+}
--- Forge/src/main/java/dhyces/contextually/client/contexts/KeyContextManager.java
+++ Forge/src/main/java/dhyces/contextually/client/contexts/KeyContextManager.java
@@ -1,8 +1,10 @@
 package dhyces.contextually.client.contexts;
 
 import com.google.common.collect.*;
+import com.mojang.datafixers.util.Pair;
 import dhyces.contextually.ContextuallyCommon;
 import dhyces.contextually.client.contexts.conditions.Condition;
+import dhyces.contextually.client.contexts.keys.Key;
 import dhyces.contextually.client.contexts.objects.BlockKeyContext;
 import dhyces.contextually.client.contexts.objects.EntityKeyContext;
 import dhyces.contextually.client.contexts.objects.GlobalKeyContext;
@@ -20,6 +22,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.client.model.generators.VariantBlockStateBuilder;
 
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -27,17 +30,17 @@ import java.util.concurrent.Executor;
 
 public final class KeyContextManager implements PreparableReloadListener {
 
-    final DefaultingMultiMapDelegate<BlockState, IKeyContext<BlockState>> BLOCK_STATE_CONTEXTS = createDelegate(new BlockKeyContext(ContextuallyCommon.modloc("default_block_attack"), Set.of(ContextuallyCommon.modloc("mouse_left_key")), Set.of()), new BlockKeyContext(ContextuallyCommon.modloc("default_block_use"), Set.of(ContextuallyCommon.modloc("mouse_right_key")), Set.of(new Condition.PlayerHeldBlockCondition(null))));
-    final DefaultingMultiMapDelegate<EntityType<?>, IKeyContext<Entity>> ENTITY_CONTEXTS = createDelegate(new EntityKeyContext(ContextuallyCommon.modloc("default_entity_attack"), Set.of(ContextuallyCommon.modloc("mouse_left_key")), Set.of()));
-    final DefaultingMultiMapDelegate<Item, IKeyContext<ItemStack>> ITEM_CONTEXTS = createDelegate();
-    final List<IKeyContext<Player>> GLOBAL_CONTEXTS = Lists.newArrayList();
+    private DefaultingMultiMapDelegate<BlockState, IKeyContext<BlockState>> BLOCK_STATE_CONTEXTS = createDelegate(new BlockKeyContext(ContextuallyCommon.modloc("default_block_attack"), Set.of(Key.of("key.attack")), Set.of()), new BlockKeyContext(ContextuallyCommon.modloc("default_block_use"), Set.of(Key.of("key.use")), Set.of(new Condition.PlayerHeldBlockCondition(null))));
+    private DefaultingMultiMapDelegate<EntityType<?>, IKeyContext<Entity>> ENTITY_CONTEXTS = createDelegate(new EntityKeyContext(ContextuallyCommon.modloc("default_entity_attack"), Set.of(Key.of("key.attack")), Set.of()));
+    private DefaultingMultiMapDelegate<Item, IKeyContext<ItemStack>> ITEM_CONTEXTS = createDelegate();
+    private ImmutableList<IKeyContext<Player>> GLOBAL_CONTEXTS = ImmutableList.of();
 
     public KeyContextManager() {
-        BLOCK_STATE_CONTEXTS.put(Blocks.LEVER.defaultBlockState(), new BlockKeyContext(ContextuallyCommon.modloc("lever_use"), Set.of(ContextuallyCommon.modloc("mouse_right_key")), Set.of()));
-        BLOCK_STATE_CONTEXTS.put(Blocks.WATER.defaultBlockState(), new BlockKeyContext(ContextuallyCommon.modloc("bucket_water"), Set.of(ContextuallyCommon.modloc("mouse_right_key")), Set.of(new Condition.PlayerHeldItemCondition(Items.BUCKET, null))));
-        ENTITY_CONTEXTS.put(EntityType.VILLAGER, new EntityKeyContext(ContextuallyCommon.modloc("villager_use"), Set.of(ContextuallyCommon.modloc("mouse_right_key")), Set.of(new Condition.NotCondition(new Condition.VillagerProfessionCondition("none")))));
-        ENTITY_CONTEXTS.put(EntityType.COW, new EntityKeyContext(ContextuallyCommon.modloc("cow_use"), Set.of(ContextuallyCommon.modloc("mouse_right_key")), Set.of(new Condition.PlayerHeldItemCondition(Items.BUCKET, null))));
-        GLOBAL_CONTEXTS.add(new GlobalKeyContext(ContextuallyCommon.modloc("open_inventory"), Set.of(ContextuallyCommon.modloc("e_key")), Set.of()));
+//        BLOCK_STATE_CONTEXTS.put(Blocks.LEVER.defaultBlockState(), new BlockKeyContext(ContextuallyCommon.modloc("lever_use"), Set.of(Key.of("key.use")), Set.of()));
+//        BLOCK_STATE_CONTEXTS.put(Blocks.WATER.defaultBlockState(), new BlockKeyContext(ContextuallyCommon.modloc("bucket_water"), Set.of(Key.of("key.use")), Set.of(new Condition.PlayerHeldItemCondition(Items.BUCKET, null))));
+//        ENTITY_CONTEXTS.put(EntityType.VILLAGER, new EntityKeyContext(ContextuallyCommon.modloc("villager_use"), Set.of(Key.of("key.use")), Set.of(new Condition.NotCondition(new Condition.VillagerProfessionCondition("none")))));
+//        ENTITY_CONTEXTS.put(EntityType.COW, new EntityKeyContext(ContextuallyCommon.modloc("cow_use"), Set.of(Key.of("key.use")), Set.of(new Condition.PlayerHeldItemCondition(Items.BUCKET, null))));
+//        GLOBAL_CONTEXTS.add(new GlobalKeyContext(ContextuallyCommon.modloc("open_inventory"), Set.of(Key.of("key.inventory")), Set.of()));
     }
 
     public boolean hasContextForBlock(BlockState block) {
@@ -69,7 +72,7 @@ public final class KeyContextManager implements PreparableReloadListener {
     }
 
     public Collection<IKeyContext<Player>> getGlobalContexts() {
-        return Collections.unmodifiableCollection(GLOBAL_CONTEXTS);
+        return GLOBAL_CONTEXTS;
     }
 
     private <K, V> DefaultingMultiMapDelegate<K, V> createDelegate(V... contexts) {
@@ -78,6 +81,38 @@ public final class KeyContextManager implements PreparableReloadListener {
 
     @Override
     public CompletableFuture<Void> reload(PreparationBarrier pPreparationBarrier, ResourceManager pResourceManager, ProfilerFiller pPreparationsProfiler, ProfilerFiller pReloadProfiler, Executor pBackgroundExecutor, Executor pGameExecutor) {
-        return CompletableFuture.supplyAsync(() -> new KeyContextLoader(pResourceManager)).thenCompose(pPreparationBarrier::wait).thenAcceptAsync(loader -> loader.load(), pGameExecutor);
+        return CompletableFuture.supplyAsync(() -> new KeyContextLoader(pResourceManager))
+                .thenCompose(pPreparationBarrier::wait)
+                .thenAcceptAsync(loader -> {
+                    Multimap<BlockState, IKeyContext<BlockState>> blockMap = ArrayListMultimap.create();
+                    Collection<IKeyContext<BlockState>> blockDefault = Lists.newArrayList();
+                    Multimap<EntityType<?>, IKeyContext<Entity>> entityMap = ArrayListMultimap.create();
+                    Collection<IKeyContext<Entity>> entityDefault = Lists.newArrayList();
+                    ImmutableList.Builder<IKeyContext<Player>> global = ImmutableList.builder();
+                    for (Pair<? extends Collection<?>, ? extends IKeyContext<?>> pair : loader.load()) {
+                        var collection = pair.getFirst();
+                        var context = pair.getSecond();
+                        if (context instanceof BlockKeyContext blockKeyContext) {
+                            if (collection.isEmpty()) {
+                                blockDefault.add(blockKeyContext);
+                            }
+                            for (Object state : collection) {
+                                blockMap.put((BlockState)state, blockKeyContext);
+                            }
+                        } else if (context instanceof EntityKeyContext entityKeyContext) {
+                            if (collection.isEmpty()) {
+                                entityDefault.add(entityKeyContext);
+                            }
+                            for (Object entity : collection) {
+                                entityMap.put((EntityType<?>)entity, entityKeyContext);
+                            }
+                        } else if (context instanceof GlobalKeyContext globalKeyContext) {
+                            global.add(globalKeyContext);
+                        }
+                    }
+                    BLOCK_STATE_CONTEXTS = new DefaultingMultiMapDelegate<>(blockMap, blockDefault);
+                    ENTITY_CONTEXTS = new DefaultingMultiMapDelegate<>(entityMap, entityDefault);
+                    GLOBAL_CONTEXTS = global.build();
+                }, pGameExecutor);
     }
 }
