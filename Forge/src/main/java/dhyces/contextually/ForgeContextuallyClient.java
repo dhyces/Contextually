4c51faf982297fde1f54382a4a89510c026ba7dc
dhyces <pokmon987@gmail.com>
2022-07-26 09:45:00 -0400

    Change Forge's ContextuallyClient to ForgeContextuallyClient
    -will likely just make this the main mod class, unless I do some stuff for a server side aspect to the mod

---
+++ Forge/src/main/java/dhyces/contextually/ForgeContextuallyClient.java
@@ -0,0 +1,25 @@
+package dhyces.contextually;
+
+import dhyces.contextually.client.ContextuallyClient;
+import dhyces.contextually.client.gui.ContextGuiOverlay;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class ForgeContextuallyClient {
+
+    static void init(IEventBus bus) {
+        bus.addListener(ForgeContextuallyClient::registerReloadables);
+        bus.addListener(ForgeContextuallyClient::registerGuis);
+    }
+
+    private static void registerReloadables(RegisterClientReloadListenersEvent event) {
+        event.registerReloadListener(ContextuallyClient.getTextureManager());
+        event.registerReloadListener(ContextuallyClient.getContextManager());
+    }
+
+    private static void registerGuis(RegisterGuiOverlaysEvent event) {
+        event.registerAbove(new ResourceLocation("item_name"), "key_contexts", new ContextGuiOverlay());
+    }
+}
