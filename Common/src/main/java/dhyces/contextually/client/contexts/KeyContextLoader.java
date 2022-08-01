76a38069c3777f84a755758fbc037aca0dbf468b
dhyces <pokmon987@gmail.com>
2022-08-01 16:25:53 -0400

    Clean up deserialization in KeyContextLoader

--- Common/src/main/java/dhyces/contextually/client/contexts/KeyContextLoader.java
+++ Common/src/main/java/dhyces/contextually/client/contexts/KeyContextLoader.java
@@ -37,47 +37,47 @@ public class KeyContextLoader {
 
     public List<Pair<? extends Collection<?>, ? extends IKeyContext<?>>> load() {
         List<Pair<? extends Collection<?>, ? extends IKeyContext<?>>> read = new LinkedList<>();
-        for (Map.Entry<ResourceLocation, Resource> entry : resourceManager.listResources("contexts", resourceLocation -> resourceLocation.getPath().endsWith(".json")).entrySet()) {
+        for (Map.Entry<ResourceLocation, Resource> entry : resourceManager.listResources("contexts", resourceLocation -> resourceLocation.getPath().endsWith(fileExtension)).entrySet()) {
             try(var reader = entry.getValue().openAsReader()) {
                 var jsonElement = JsonParser.parseReader(reader);
                 // get the object of the file
                 // iterate through context serializer entries to find matching targets
                 // TODO: redo this whole process
                 var obj = jsonElement.getAsJsonObject();
-                try {
-                    var loader = obj.get("loader").getAsString();
-                    var loaderKey = loader.contains(":") ? ResourceLocation.of(loader, ':') : ContextuallyCommon.id(loader);
-                    var serializer = CONTEXT_SERIALIZERS.get(loaderKey);
-                    try {
-                        Objects.requireNonNull(serializer, "Loader \"" + loaderKey + "\" does not exist.");
-                        var path = entry.getKey();
-                        var id = new ResourceLocation(path.getNamespace(), path.getPath().substring(0, path.getPath().indexOf(fileExtension)));
-                        var context = serializer.deserialize(id, obj);
-                        Objects.requireNonNull(context, "Deserializer \"" + loaderKey + "\" returned null context.");
-                        read.add(context);
-                    } catch (NullPointerException e) {
-                        ContextuallyCommon.LOGGER.error(e.getMessage());
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+                var loaderJson = obj.get("loader");
+                checkParse(loaderJson != null, "Key \"loader\" must not be null. Must be a valid loader id.");
+                checkParse(!loaderJson.isJsonPrimitive() || !loaderJson.getAsJsonPrimitive().isString(), "Value of key \"loader\" must be a valid string.");
+                var loader = loaderJson.getAsString();
+                var loaderKey = loader.contains(":") ? ResourceLocation.of(loader, ':') : ContextuallyCommon.id(loader);
+                var serializer = CONTEXT_SERIALIZERS.get(loaderKey);
+                checkParse(serializer != null,"Loader \"" + loaderKey + "\" does not exist.");
+                var path = entry.getKey();
+                var id = new ResourceLocation(path.getNamespace(), path.getPath().substring(0, path.getPath().indexOf(fileExtension)));
+                var context = serializer.deserialize(id, obj);
+                Objects.requireNonNull(context, "Deserializer \"" + loaderKey + "\" returned null context. Check " + serializer);
+                read.add(context);
+            } catch (IOException | JsonParseException e) {
+                ContextuallyCommon.LOGGER.error("Failed to parse context: " + entry.getKey() + ". " + e.getMessage());
             }
         }
         return read;
     }
 
+    public static void checkParse(boolean expression, String message) {
+        if (!expression)
+            throw new JsonParseException(message);
+    }
+
     @NotNull
     public static IConditionPredicate deserializeCondition(@NotNull JsonObject conditionObject) {
-        Preconditions.checkNotNull(conditionObject);
-        var conditionId = conditionObject.get("condition").getAsString();
-        try {
-            var serializer = CONDITION_SERIALIZERS.get(ResourceLocation.of(conditionId, ':'));
-            return serializer.deserialize(conditionObject);
-        } catch (NullPointerException e) {
-            throw new IllegalArgumentException("No serializer for condition: " + conditionId + " exists.");
-        }
+        checkParse(conditionObject != null, "Object in \"conditions\" array is null.");
+        var conditionJson = conditionObject.get("condition");
+        checkParse(conditionJson != null, "Key \"condition\" must not be null. Must be a valid condition id.");
+        checkParse(!conditionJson.isJsonPrimitive() || !conditionJson.getAsJsonPrimitive().isString(), "Value of key \"condition\" must be a valid string.");
+        var conditionId = conditionJson.getAsString();
+        var serializer = CONDITION_SERIALIZERS.get(ContextuallyCommon.defaultingId(conditionId));
+        checkParse(serializer != null, "Condition serializer \"" + conditionId + "\" does not exist.");
+        return serializer.deserialize(conditionObject);
     }
 
     @NotNull
@@ -91,22 +91,17 @@ public class KeyContextLoader {
     }
 
     @NotNull
-    public static IIcon deserializeIcon(JsonObject keyObject) {
-        Objects.requireNonNull(keyObject);
-        var type = keyObject.get("type");
+    public static IIcon deserializeIcon(JsonObject iconObject) {
+        checkParse(iconObject != null, "Object in \"icons\" array is null.");
+        var type = iconObject.get("type");
 
-        if (type == null)
-            throw new JsonParseException("key \"type\" is must be defined");
+        checkParse(type != null, "Key \"type\" is must be defined.");
+        checkParse(!type.isJsonPrimitive() || !type.getAsJsonPrimitive().isString(), "Value of key \"type\" must be a valid string.");
 
-        if (!(type instanceof JsonPrimitive primitive && primitive.isString()))
-            throw new JsonParseException("key \"type\" is expected to be a string.");
-
-        var key = ContextuallyCommon.modDefaultingloc(type.getAsString());
-        if (ICON_SERIALIZERS_MAP.containsKey(key)) {
-            var serializer = ICON_SERIALIZERS_MAP.get(key);
-            return serializer.deserialize(keyObject);
-        }
-        throw new IllegalArgumentException("No serializer for icon: " + type + " exists.");
+        var icon = ContextuallyCommon.defaultingId(type.getAsString());
+        var serializer = ICON_SERIALIZERS_MAP.get(icon);
+        checkParse(serializer != null, "Icon serializer \"" + icon + "\" does not exist.");
+        return serializer.deserialize(iconObject);
     }
 
     @NotNull
