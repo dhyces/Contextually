1d7035528c5c1cf16921de4dcb90c1de88054dbe
dhyces <pokmon987@gmail.com>
2022-08-01 19:43:29 -0400

    Fix AnimatedIcon again

--- Common/src/main/java/dhyces/contextually/client/contexts/icons/AnimatedIcon.java
+++ Common/src/main/java/dhyces/contextually/client/contexts/icons/AnimatedIcon.java
@@ -2,47 +2,30 @@ package dhyces.contextually.client.contexts.icons;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import dhyces.contextually.ContextuallyCommon;
-import dhyces.contextually.services.Services;
 import dhyces.contextually.util.IntPair;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class AnimatedIcon implements IIcon {
-
-    final List<IntPair<IIcon>> icons;
-
-    float index;
+public record AnimatedIcon(List<IntPair<IIcon>> icons, int maxTicks) implements IIcon {
 
     public AnimatedIcon(@NotNull List<IntPair<IIcon>> icons) {
-        this.icons = icons;
-    }
-
-    public List<IntPair<IIcon>> getIcons() {
-        return icons;
+        this(icons.stream().sorted(Comparator.comparingInt(IntPair::value)).toList(), icons.stream().mapToInt(IntPair::value).sum());
     }
 
     @Override
     public void render(Gui gui, PoseStack poseStack, float partialTicks, int blitOffset, int x, int y, int width, int height) {
-        tick(gui, partialTicks);
-        var animatedObject = icons.get((int)index);
-        animatedObject.object().render(gui, poseStack, partialTicks, blitOffset, x, y, width, height);
-        // increment tickPos
-        // check if it's equal to the current object's tickMax or TICK_MAX
-        //   true: reset tickPos to 0 and increment/reset index
-        //   false: do nothing
-
-    }
-
-    private void tick(Gui gui, float partialTicks) {
-        var progress = Mth.lerp(partialTicks, (int)index, ((int)index)+1);
-        if (progress < index && gui.getGuiTicks() % icons.get((int)index).value() == 0) {
-            index = ((int)index+1) % icons.size();
-        } else {
-            index = progress;
+        int ticks = 0;
+        var fragmentedTicks = gui.getGuiTicks() % maxTicks;
+        for (IntPair<IIcon> pair : icons) {
+            ticks += pair.value();
+            if (fragmentedTicks < ticks) {
+                pair.object().render(gui, poseStack, partialTicks, blitOffset, x, y, width, height);
+                break;
+            }
         }
     }
 
