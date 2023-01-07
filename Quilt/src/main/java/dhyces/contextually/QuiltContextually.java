afe173da977c93a01359548819e7baa5ced57620
dhyces <pokmon987@gmail.com>
2023-01-07 17:43:26 -0500

    Basic quilt things

--- Quilt/src/main/java/dhyces/contextually/QuiltContextually.java
+++ Quilt/src/main/java/dhyces/contextually/QuiltContextually.java
@@ -1,5 +1,6 @@
 package dhyces.contextually;
 
+import dhyces.contextually.client.ContextGui;
 import dhyces.contextually.client.ContextuallyClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
@@ -10,6 +11,7 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import org.jetbrains.annotations.NotNull;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.lifecycle.api.client.event.ClientTickEvents;
 import org.quiltmc.qsl.resource.loader.api.ResourceLoader;
 import org.quiltmc.qsl.resource.loader.api.client.ClientResourceLoaderEvents;
 import org.quiltmc.qsl.resource.loader.api.reloader.IdentifiableResourceReloader;
@@ -19,17 +21,26 @@ import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 
 public class QuiltContextually implements ClientModInitializer {
+
+    private static ContextGui contextHud;
+
     @Override
     public void onInitializeClient(ModContainer mod) {
         ClientResourceLoaderEvents.START_RESOURCE_PACK_RELOAD.register(this::clientInit);
         ResourceLoader.get(PackType.CLIENT_RESOURCES).registerReloader(new WrappedReloader("key_textures", ContextuallyClient::getTextureManager));
         ResourceLoader.get(PackType.CLIENT_RESOURCES).registerReloader(new WrappedReloader("contexts", ContextuallyClient::getContextManager));
+        ClientTickEvents.START.register(this::guiRenderTick);
     }
 
     private void clientInit(Minecraft client, ResourceManager resourceManager, boolean first) {
         if (first) {
             ContextuallyClient.init();
+            contextHud = new ContextGui(client, client.getItemRenderer());
         }
+    }
+
+    private void guiRenderTick(Minecraft client) {
+        contextHud.tick(client.isPaused());
     }
 
     class WrappedReloader implements IdentifiableResourceReloader {
---
+++ Quilt/src/main/java/dhyces/contextually/client/ContextGui.java
@@ -0,0 +1,12 @@
+package dhyces.contextually.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+
+public class ContextGui extends Gui {
+
+    public ContextGui(Minecraft minecraft, ItemRenderer itemRenderer) {
+        super(minecraft, itemRenderer);
+    }
+}
