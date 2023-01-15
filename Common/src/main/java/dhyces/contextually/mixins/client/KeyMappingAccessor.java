151ef1fcf4db3843231fb4f2f6c743dec783162e
dhyces <pokmon987@gmail.com>
2023-01-15 17:35:29 -0500

    Remove the silly Fabric KeyMappingMixin
    We just use an Accessor in common

--- Common/src/main/java/dhyces/contextually/mixins/client/KeyMappingAccessor.java
+++ Common/src/main/java/dhyces/contextually/mixins/client/KeyMappingAccessor.java
@@ -4,15 +4,11 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(KeyMapping.class)
-public class KeyMappingMixin implements IKeyMappingMixin {
+public interface KeyMappingAccessor {
 
-    @Shadow
-    private InputConstants.Key key;
-
-    @Override
-    public InputConstants.Key getKey() {
-        return key;
-    }
+    @Accessor
+    InputConstants.Key getKey();
 }
--- Fabric/src/main/java/dhyces/contextually/mixins/client/IKeyMappingMixin.java
+++
@@ -1,7 +0,0 @@
-package dhyces.contextually.mixins.client;
-
-import com.mojang.blaze3d.platform.InputConstants;
-
-public interface IKeyMappingMixin {
-    InputConstants.Key getKey();
-}
