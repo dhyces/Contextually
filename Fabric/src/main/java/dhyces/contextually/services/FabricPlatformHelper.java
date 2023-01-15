90127f7c93e56191ef891dcae0af40eeb9a5cad6
dhyces <pokmon987@gmail.com>
2023-01-15 17:35:38 -0500

    Update FabricPlatformHelper.java

--- Fabric/src/main/java/dhyces/contextually/services/FabricPlatformHelper.java
+++ Fabric/src/main/java/dhyces/contextually/services/FabricPlatformHelper.java
@@ -1,12 +1,14 @@
 package dhyces.contextually.services;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import dhyces.contextually.mixins.client.IKeyMappingMixin;
+import dhyces.contextually.mixins.client.InputConstantsTypeAccessor;
+import dhyces.contextually.mixins.client.KeyMappingAccessor;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.client.KeyMapping;
 
 public class FabricPlatformHelper implements IPlatformHelper {
     @Override
     public InputConstants.Key getKey(KeyMapping keyMapping) {
-        return ((IKeyMappingMixin)keyMapping).getKey();
+        return ((KeyMappingAccessor)keyMapping).getKey();
     }
 }
