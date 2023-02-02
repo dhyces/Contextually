2d069050ca6d06a324bc8c19259de54597ad5ec2
dhyces <pokmon987@gmail.com>
2023-02-02 11:55:55 -0500

    Fix potential issues with texture icons

--- Common/src/main/java/dhyces/contextually/client/core/icons/objects/KeyIcon.java
+++ Common/src/main/java/dhyces/contextually/client/core/icons/objects/KeyIcon.java
@@ -1,11 +1,15 @@
 package dhyces.contextually.client.core.icons.objects;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.serialization.Codec;
 import dhyces.contextually.client.ContextuallyClient;
 import dhyces.contextually.client.core.icons.IIconType;
 import dhyces.contextually.client.core.icons.ITextureIcon;
 import dhyces.contextually.client.keys.IKey;
+import dhyces.contextually.client.textures.KeyMappingTextureManager;
 import dhyces.contextually.util.MoreCodecs;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
 public record KeyIcon(IKey key) implements ITextureIcon {
@@ -13,6 +17,13 @@ public record KeyIcon(IKey key) implements ITextureIcon {
             KeyIcon::new,
             KeyIcon::key
             ).codec();
+
+    @Override
+    public void render(Gui gui, PoseStack poseStack, float partialTicks, int blitOffset, int x, int y, int width, int height) {
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.setShaderTexture(0, KeyMappingTextureManager.KEYS);
+        ITextureIcon.super.render(gui, poseStack, partialTicks, blitOffset, x, y, width, height);
+    }
 
     @Override
     public TextureAtlasSprite retrieveTexture() {
--- Common/src/main/java/dhyces/contextually/client/core/icons/objects/KeyTextureIcon.java
+++ Common/src/main/java/dhyces/contextually/client/core/icons/objects/KeyTextureIcon.java
@@ -1,14 +1,25 @@
 package dhyces.contextually.client.core.icons.objects;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.serialization.Codec;
 import dhyces.contextually.client.ContextuallyClient;
 import dhyces.contextually.client.core.icons.IIconType;
 import dhyces.contextually.client.core.icons.ITextureIcon;
+import dhyces.contextually.client.textures.KeyMappingTextureManager;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
 
 public record KeyTextureIcon(ResourceLocation texture) implements ITextureIcon {
     public static final Codec<KeyTextureIcon> CODEC = ResourceLocation.CODEC.fieldOf("key_texture").xmap(KeyTextureIcon::new, KeyTextureIcon::texture).codec();
+
+    @Override
+    public void render(Gui gui, PoseStack poseStack, float partialTicks, int blitOffset, int x, int y, int width, int height) {
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.setShaderTexture(0, KeyMappingTextureManager.KEYS);
+        ITextureIcon.super.render(gui, poseStack, partialTicks, blitOffset, x, y, width, height);
+    }
 
     @Override
     public TextureAtlasSprite retrieveTexture() {
