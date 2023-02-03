a1235dd2be2cd475f63938a0c0db8127387289cc
dhyces <pokmon987@gmail.com>
2023-02-03 17:21:49 -0500

    Cool new tests

--- Forge/src/test/java/dhyces/testmod/TestMod.java
+++ Forge/src/test/java/dhyces/testmod/TestMod.java
@@ -1,15 +1,13 @@
 package dhyces.testmod;
 
-import dhyces.contextually.client.core.contexts.IKeyContext;
-import dhyces.contextually.client.core.contexts.objects.ItemKeyContext;
-import dhyces.contextually.common.data.ContextProvider;
 import dhyces.testmod.data.TestContextProvider;
+import dhyces.testmod.data.VanillaContextProvider;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.data.loading.DatagenModLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import java.util.function.BiConsumer;
 
 @Mod("testmod")
 public class TestMod {
@@ -21,14 +19,11 @@ public class TestMod {
     }
 
     private void addProviders(final GatherDataEvent event) {
-        var packOutput = event.getGenerator().getPackOutput();
-        var fileHelper = event.getExistingFileHelper();
+        PackOutput packOutput = event.getGenerator().getPackOutput();
+        ExistingFileHelper fileHelper = event.getExistingFileHelper();
+        VanillaContextProvider.VanillaContextEN_USLangProvider en_usLangProvider = new VanillaContextProvider.VanillaContextEN_USLangProvider(packOutput);
+        event.getGenerator().addProvider(event.includeClient(), new VanillaContextProvider(en_usLangProvider, packOutput, fileHelper));
+//        event.getGenerator().addProvider(event.includeClient(), en_usLangProvider);
         event.getGenerator().addProvider(event.includeClient(), new TestContextProvider(packOutput, fileHelper, "testmod"));
-        event.getGenerator().addProvider(event.includeClient(), new ContextProvider(packOutput, fileHelper, "addendum") {
-            @Override
-            protected void addContexts(BiConsumer<String, IKeyContext<?, ?>> exporter) {
-                ContextProvider.ContextBuilder.create(ItemKeyContext::new).addCondition(ContextProvider.ConditionFactories.heldKey(49)).export("testing123", exporter);
-            }
-        });
     }
 }
--- Forge/src/test/java/dhyces/testmod/data/TestContextProvider.java
+++ Forge/src/test/java/dhyces/testmod/data/TestContextProvider.java
@@ -3,13 +3,16 @@ package dhyces.testmod.data;
 import com.mojang.blaze3d.platform.InputConstants;
 import dhyces.contextually.client.core.conditions.predicates.ItemContextPredicate;
 import dhyces.contextually.client.core.contexts.IKeyContext;
+import dhyces.contextually.client.core.contexts.PartialBlockState;
 import dhyces.contextually.client.core.contexts.objects.BlockKeyContext;
 import dhyces.contextually.client.core.contexts.objects.EntityKeyContext;
 import dhyces.contextually.client.core.contexts.objects.GlobalKeyContext;
 import dhyces.contextually.client.core.contexts.objects.ItemKeyContext;
 import dhyces.contextually.common.data.ContextProvider;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.VillagerProfession;
@@ -18,23 +21,26 @@ import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.AnvilBlock;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LecternBlock;
+import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.level.block.state.properties.RedstoneSide;
+import net.minecraft.world.level.redstone.Redstone;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.BiConsumer;
 
 public class TestContextProvider extends ContextProvider {
 
     public TestContextProvider(@Nonnull PackOutput output, @Nonnull ExistingFileHelper fileHelper, @Nonnull String modid) {
-        super(output, fileHelper, modid);
+        super(null, output, fileHelper, modid);
     }
 
     AtomicInteger integer = new AtomicInteger();
 
     @Override
-    protected void addContexts(BiConsumer<String, IKeyContext<?, ?>> exporter) {
+    protected void addContexts(ContextExporter exporter) {
         // 0
         ContextBuilder.create(ItemKeyContext::new)
                 .addIcon(IconFactories.itemIcon(Items.GRANITE))
@@ -138,10 +144,16 @@ public class TestContextProvider extends ContextProvider {
                 .export(intName(), exporter);
 
         // 13
-        ContextBuilder.create(BlockKeyContext::new)
+        BlockContextBuilder.create()
                 .addIcon(IconFactories.keyIcon("key.crouch"))
-                .addTarget(Blocks.TARGET.defaultBlockState())
-                .addTarget(Blocks.ANVIL.defaultBlockState().setValue(AnvilBlock.FACING, Direction.NORTH))
+                .addBlock(Blocks.TARGET)
+                .addPartialStates(
+                        List.of(
+                                PartialBlockState.builder(Blocks.LECTERN).with(LecternBlock.FACING, Direction.NORTH).build(),
+                                PartialBlockState.builder(Blocks.LECTERN).with(LecternBlock.FACING, Direction.SOUTH).with(LecternBlock.HAS_BOOK, true).build(),
+                                PartialBlockState.builder(Blocks.LECTERN).with(LecternBlock.FACING, Direction.EAST).with(LecternBlock.HAS_BOOK, false).build()
+                        )
+                )
                 .export(intName(), exporter);
 
         // 14
@@ -160,6 +172,51 @@ public class TestContextProvider extends ContextProvider {
                         .build())
                 .addTarget(EntityType.AXOLOTL)
                 .addTarget(EntityType.CAT)
+                .export(intName(), exporter);
+
+        // 16
+        BlockContextBuilder.create()
+                .addIcon(IconFactories.animatedIconBuilder()
+                        .iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0x0000FF), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0x0055AA), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0x00AA55), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0x00FF00), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0x55AA00), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0xAA5500), 1
+                        ).iconForTime(
+                                IconFactories.textureAtlasIcon(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation("item/stone_pickaxe"), 0xFF0000), 1
+                        ).build()
+                )
+                .addBlock(Blocks.GRAY_GLAZED_TERRACOTTA)
+                .export(intName(), exporter);
+
+        // 17
+        BlockContextBuilder.create()
+                .addIcon(IconFactories.keyIcon("key.crouch"))
+                .addPartialStates(
+                        List.of(
+                                PartialBlockState.builder(Blocks.REDSTONE_WIRE).with(RedStoneWireBlock.EAST, RedstoneSide.UP).build(),
+                                PartialBlockState.builder(Blocks.REDSTONE_WIRE).with(RedStoneWireBlock.EAST, RedstoneSide.SIDE).build(),
+                                PartialBlockState.builder(Blocks.REDSTONE_WIRE).with(RedStoneWireBlock.WEST, RedstoneSide.UP).build()
+                        )
+                )
+                .export(intName(), exporter);
+
+        // 18
+        BlockContextBuilder.create()
+                .addIcon(IconFactories.keyIcon("key.crouch"))
+                .addPartialStates(
+                        List.of(
+                                PartialBlockState.builder(Blocks.LECTERN).with(LecternBlock.FACING, Direction.EAST).with(LecternBlock.HAS_BOOK, true).with(LecternBlock.POWERED, false).build(),
+                                PartialBlockState.builder(Blocks.LECTERN).with(LecternBlock.FACING, Direction.EAST).with(LecternBlock.HAS_BOOK, false).build()
+                        )
+                )
                 .export(intName(), exporter);
     }
 
